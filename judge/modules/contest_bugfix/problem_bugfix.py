7a7eb1107d2ed3812b51d22b158046d3e7106466
Jack Chen <jachen2@stanford.edu>
2014-05-18 14:22:28 -0700

    Quick bugfix for else if

--- judge/modules/contest_bugfix/problem_bugfix.py
+++ judge/modules/contest_bugfix/problem_bugfix.py
@@ -13,7 +13,7 @@ from utils import GradingException
 import common
 
 MAX_LINE_EDIT_DISTANCE = 200
-CONTROL_FLOW_KEYWORDS = ['if', 'else', 'for', 'while', 'do', 'switch'] # for java and c/c++
+CONTROL_FLOW_KEYWORDS = ['if', 'for', 'while', 'do', 'switch'] # for java and c/c++
 
 # Splits a source code string into lines, removing empty lines and trailing whitespace
 def get_clean_lines(code):
